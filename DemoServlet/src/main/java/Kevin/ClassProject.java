93afa66970c9fe50b048ddfde147f65c81c923ce
Gabriel Barbera <80081651+g-barbera@users.noreply.github.com>
2023-11-27 04:00:38 +0000

    improved formatting in ClassProject
    improved formatting

--- DemoServlet/src/main/java/Kevin/ClassProject.java
+++ DemoServlet/src/main/java/Kevin/ClassProject.java
@@ -12,64 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ClassProject")
 public class ClassProject extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	 private final String question = "What is the capital of France?";
-	    private final String[] choices = { "Paris", "Berlin", "London", "Rome" };
+ 	private final String question = "What is the capital of France?";
+	private final String[] choices = { "Paris", "Berlin", "London", "Rome" };
 
-	    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	            throws ServletException, IOException {
-	        // Get the selected answer from the client
-	        String selectedAnswer = request.getParameter("selectedAnswer");
-	        String result = processAnswer(selectedAnswer);
-
-	        // Send the result back to the client as HTML
-	        response.getWriter().write("<html><body>");
-	        response.getWriter().write("<h1>Question</h1>");
-	        response.getWriter().write("<p>" + question + "</p>");
-
-	        // Display the selected answer and result
-	        response.getWriter().write("<p>You selected: " + selectedAnswer + "</p>");
-	        response.getWriter().write("<p>Result: " + result + "</p>");
-
-	        // Display the choices again
-	        displayChoices(response);
-
-	        response.getWriter().write("</body></html>");
-	    }
-	    private String processAnswer(String answer) {
-	        // Perform server-side processing here
-	        // For simplicity, just checking if the answer is correct
-	        return (answer.equals("Paris")) ? "Correct!" : "Incorrect!";
-	    }
-	    private void displayChoices(HttpServletResponse response) throws IOException {
-	        response.getWriter().write("<form method=\"post\">");
-
-	        for (String choice : choices) {
-	            response.getWriter().write("<input type=\"radio\" name=\"selectedAnswer\" value=\"" + choice + "\">");
-	            response.getWriter().write(choice + "<br>");
-	        }
-
-	        response.getWriter().write("<br>");
-	        response.getWriter().write("<button type=\"submit\">Submit Answer</button>");
-	        response.getWriter().write("</form>");
-	    }
-	    @Override
-	    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	            throws ServletException, IOException {
-	        response.setContentType("text/html");
-	        response.getWriter().write("<!DOCTYPE html>");
-	        response.getWriter().write("<html>");
-	        response.getWriter().write("<head><title>Question Picker</title></head>");
-	        response.getWriter().write("<body>");
-
-	        response.getWriter().write("<h1>Question</h1>");
-	        response.getWriter().write("<p>" + question + "</p>");
-
-	        // Display choices for the initial page
-	        displayChoices(response);
-
-	        response.getWriter().write("</body>");
-	        response.getWriter().write("</html>");
-	    }
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -78,4 +23,57 @@ public class ClassProject extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Get the selected answer from the client
+		String selectedAnswer = request.getParameter("selectedAnswer");
+		String result = processAnswer(selectedAnswer);
+
+		// Send the result back to the client as HTML
+		response.getWriter().write("<html><body>");
+		response.getWriter().write("<h1>Question</h1>");
+		response.getWriter().write("<p>" + question + "</p>");
+
+		// Display the selected answer and result
+		response.getWriter().write("<p>You selected: " + selectedAnswer + "</p>");
+		response.getWriter().write("<p>Result: " + result + "</p>");
+
+		// Display the choices again
+		displayChoices(response);
+
+		response.getWriter().write("</body></html>");
+	}
+	private String processAnswer(String answer) {
+		// Perform server-side processing here
+		// For simplicity, just checking if the answer is correct
+		return (answer.equals("Paris")) ? "Correct!" : "Incorrect!";
+	}
+	private void displayChoices(HttpServletResponse response) throws IOException {
+		response.getWriter().write("<form method=\"post\">");
+
+		for (String choice : choices) {
+			response.getWriter().write("<input type=\"radio\" name=\"selectedAnswer\" value=\"" + choice + "\">");
+			response.getWriter().write(choice + "<br>");
+		}
+
+		response.getWriter().write("<br>");
+		response.getWriter().write("<button type=\"submit\">Submit Answer</button>");
+		response.getWriter().write("</form>");
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.getWriter().write("<!DOCTYPE html>");
+		response.getWriter().write("<html>");
+		response.getWriter().write("<head><title>Question Picker</title></head>");
+		response.getWriter().write("<body>");
+		
+		response.getWriter().write("<h1>Question</h1>");
+		response.getWriter().write("<p>" + question + "</p>");
+		
+		// Display choices for the initial page
+		displayChoices(response);
+		
+		response.getWriter().write("</body>");
+		response.getWriter().write("</html>");
+	}
 }
